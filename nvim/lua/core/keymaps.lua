40a12ebacd5fc72cf5c7224c5d48158b2bab839e
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2026-05-27 03:44:42 +0900

    Fix buffer move keymaps
    - <c-b>h or <c-b>l is conflict with <c-b> (scroll up)

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -11,8 +11,8 @@ vim.keymap.set("v", "<", "<gv", { desc = "Indent decrease" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent increase" })
 
 -- buffer, tab
-vim.keymap.set("n", "<C-b>h", ":bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<C-b>l", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-f>h", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-f>l", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<C-t>h", ":tabprevious<CR>", { desc = "Previous tab" })
 vim.keymap.set("n", "<C-t>l", ":tabnext<CR>", { desc = "Next tab" })
 vim.keymap.set("n", "<Left>", ":bprevious<CR>", { desc = "Previous buffer" })
