ab98ce1ef0bb4c74c463056b2afcd284d52d2e9a
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2026-05-27 03:15:53 +0900

    formating

--- nvim/lua/plugins/edit.lua
+++ nvim/lua/plugins/edit.lua
@@ -11,12 +11,12 @@ require("nvim-autopairs").setup({
 require("mini.surround").setup({
   highlight_duration = 1500,
   mappings = {
-    add = 'sa', -- Add surrounding in Normal and Visual modes
-    delete = 'sd', -- Delete surrounding
-    find = 'sf', -- Find surrounding (to the right)
-    find_left = 'sF', -- Find surrounding (to the left)
-    highlight = 'sh', -- Highlight surroundwng
-    replace = 'sr', -- Replace surroundwng
+    add = 'sa',        -- Add surrounding in Normal and Visual modes
+    delete = 'sd',     -- Delete surrounding
+    find = 'sf',       -- Find surrounding (to the right)
+    find_left = 'sF',  -- Find surrounding (to the left)
+    highlight = 'sh',  -- Highlight surroundwng
+    replace = 'sr',    -- Replace surroundwng
 
     suffix_last = 'l', -- Suffix to search with "prev" method
     suffix_next = 'n', -- Suffix to search with "next" method
