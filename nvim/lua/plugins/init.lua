b6a3acc82ea8fccd00f4792163e3453117d96332
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2026-05-27 02:45:29 +0900

    Add nvim-treesitter-textobjects plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/nvim-mini/mini.icons" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
