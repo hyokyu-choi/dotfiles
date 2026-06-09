0a19e82ba5a8b73b0b010555504d859e6565ff03
Hyokyu Choi <chlgyrb111@uos.ac.kr>
2026-06-09 16:26:33 +0900

    refactor(nvim): change default terminal layout to float

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,6 +1,6 @@
 require("toggleterm").setup({
   open_mapping = [[<c-\>]],
-  direction = "tab",   -- "vertical" | "horizontal" | "tab" | "float"
+  direction = "float",   -- "vertical" | "horizontal" | "tab" | "float"
   highlights = {
     Normal = {
       link = "Normal",
